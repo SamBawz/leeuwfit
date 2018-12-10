d26ee3c720ec6b352ba28089b0ee019fb4ac620b
Leroy Sieberichs <lsieberichs@gmail.com>
2018-12-10 15:26:07 +0100

    News articles added

--- index.php
+++ index.php
@@ -57,6 +57,49 @@
       </div>
     </div>
     <div id="about" class="page">
+	<!--Content first paralax image-->
+	<div class="container">
+	<div class="row">
+		<div class="container">
+	<div class="row">
+	    <div class="col-md-4">
+    		<div class="card mb">
+              <img class="card-img-top" src="http://www.placehold.it/286x180" alt="Card image cap">
+              <div class="card-body mb">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" class="btn btn-primary mb">button</button>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+    		<div class="card mb">
+              <img class="card-img-top" src="http://www.placehold.it/286x180" alt="Card image cap">
+              <div class="card-body mb">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" class="btn btn-primary mb">button</button>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+    		<div class="card mb">
+              <img class="card-img-top" src="http://www.placehold.it/286x180" alt="Card image cap">
+              <div class="card-body mb">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" class="btn btn-primary mb">button</button>
+              </div>
+            </div>
+        </div>
+	</div>
+	<!--End content first paralax image-->
+</div>
+	</div>
+</div>
+
+
+
     </div>
       <div id="extra" class="page">
       </div>
